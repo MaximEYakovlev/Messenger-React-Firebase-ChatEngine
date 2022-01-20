6535e3ccb7dce66877c7838322fc1451d5f8b08c
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-20 21:04:22 +0300

    created component: Login, added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-     <Routes>
-        <Route path="/" element={< />} />
-        <Route path="/" element={< />} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={< />} /> */}
       </Routes>
     </div>
   );
