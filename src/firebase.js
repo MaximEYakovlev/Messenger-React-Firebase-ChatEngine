a81c2d213990da5a3b1bbbd8125a3336542aeb23
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-20 23:50:16 +0300

    connected firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase
+  .initializeApp({
+    apiKey: "AIzaSyCUeC2x4XPJoP1wRSv9HQuzTdIKtBgYz08",
+    authDomain: "unichat-a7c57.firebaseapp.com",
+    projectId: "unichat-a7c57",
+    storageBucket: "unichat-a7c57.appspot.com",
+    messagingSenderId: "833962996418",
+    appId: "1:833962996418:web:6fbb81831c06d8690f015b",
+  })
+  .auth();
