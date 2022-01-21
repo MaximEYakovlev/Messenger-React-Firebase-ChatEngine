65064cfa8fc21434d290be4b09e478b9384acc7d
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-21 19:54:38 +0300

    created component: Chats, connected react chat engine

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
 
 export const Chats = () => {
-  return(
-    <h1>Chats</h1>
-  )
-}
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Unichat</div>
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="d359e304-5c8b-45c2-be34-be492c1a424a"
+        userName="."
+        userSecret="."
+      />
+    </div>
+  );
+};
